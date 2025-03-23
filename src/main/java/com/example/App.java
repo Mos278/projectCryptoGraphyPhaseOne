fbf828fa23c169fe7f12326f1d0867914c04871e
Mos278 <113794284+Mos278@users.noreply.github.com>
2025-03-23 14:47:50 +0700

    phase1.0.2 -refactorForPhase2Support -renameVariablesAccordingToSpecifications -updateIsPrimeLogicForNotPrimeCheck

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,7 @@
 package com.example;
 
 import com.example.config.AppConfig;
-import com.example.service.GCD.GCDService;
-import com.example.service.exponentiation.ExponentiationService;
-import com.example.service.fileStream.FileInputStreamService;
-import com.example.service.primeNumber.PrimeNumberService;
-import java.util.Random;
+import com.example.controller.phaseOne.PhaseOneController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,92 +9,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class App {
-  private final GCDService gcdService;
-  private final PrimeNumberService primeNumberService;
-  private final ExponentiationService exponentiationService;
-  private final FileInputStreamService fileInputStreamService;
+
+  private final PhaseOneController phaseOneController;
 
   @Autowired
-  public App(
-      GCDService gcdService,
-      PrimeNumberService primeNumberService,
-      ExponentiationService exponentiationService,
-      FileInputStreamService fileInputStreamService) {
-    this.gcdService = gcdService;
-    this.primeNumberService = primeNumberService;
-    this.exponentiationService = exponentiationService;
-    this.fileInputStreamService = fileInputStreamService;
+  public App(PhaseOneController phaseOneController) {
+    this.phaseOneController = phaseOneController;
   }
 
   public void run() {
-    try {
-
-      long RandomPrimeNumber = GenPrime(31, "src/main/resources/FB_IMG.jpg");
-      long[] result = GenRandomNoWithInverse(RandomPrimeNumber);
-      System.out.println("base: " + result[0] + " inverse: " + result[1] + " prime: " + result[2]);
-      System.out.println((result[0] * result[1]) % result[2]);
-
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    phaseOneController.PresentPhase();
   }
 
   public static void main(String[] args) {
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     App app = context.getBean(App.class);
     app.run();
-  }
-
-  private long GenPrime(int bit, String fileName) throws Exception {
-    System.out.println("Reading file...");
-    long tempResult = fileInputStreamService.randomNBitFromFile(bit, fileName);
-
-    boolean forward = true;
-    long max = ((exponentiationService.fastExpo(2, bit) - 1));
-    long min = exponentiationService.fastExpo(2, (bit - 1));
-    int round = 0;
-    boolean firstBackward = true;
-
-    long Result = convertToOdd(tempResult, max);
-    System.out.println("result random from file: " + Result);
-
-    while (primeNumberService.IsPrime(Result) == PrimeNumberService.STATE_PRIME.NOT_PRIME) {
-      round++;
-      if (forward && Result + 2 < max) {
-        Result = Result + 2;
-      } else if (Result - 2 > min) {
-        forward = false;
-        if (firstBackward) {
-          Result = tempResult - 2;
-          firstBackward = false;
-        } else Result = Result - 2;
-      } else {
-        throw new Exception("Cannot find Prime Number");
-      }
-      System.out.println("Result from Gen Prime: " + Result + " round: " + round);
-    }
-    return Result;
-  }
-
-  private long[] GenRandomNoWithInverse(long prime) {
-    long base = new Random().nextLong(prime - 1) + 1;
-
-    while (gcdService.findGCD(base, prime) != 1) {
-      base++;
-      if (base >= prime) base = 1;
-    }
-    // Select base = [1 - prime - 1] && gcd(base,prime) == 1
-    long inverse = gcdService.findInverse(base, prime);
-
-    return new long[] {base, inverse, prime};
-  }
-
-  private long convertToOdd(long tempResult, long max) {
-    if (tempResult % 2 == 0) {
-      if (tempResult == max) tempResult--;
-      else tempResult++;
-    }
-    return tempResult;
   }
 }
 // ./gradlew spotlessApply
